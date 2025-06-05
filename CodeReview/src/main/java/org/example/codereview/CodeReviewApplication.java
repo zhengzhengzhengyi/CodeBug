dfaeb3ac06c844e8f68df655771ec06663cfb7f2
zy <zhengyi@piscifun.com>
2025-06-05 11:28:55 +0800

    init

---
+++ CodeReview/src/main/java/org/example/codereview/CodeReviewApplication.java
@@ -0,0 +1,13 @@
+package org.example.codereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeReviewApplication.class, args);
+    }
+
+}
